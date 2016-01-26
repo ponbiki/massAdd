6b74a1000df3e0542e7444dbe7baa4d385b07f5d
ponbiki <ponbiki@beryllium>
2016-01-26 00:14:47 -0500

    disabled replace button when no records are selected

--- src/iApiCalls.php
+++ src/iApiCalls.php
@@ -8,5 +8,5 @@ interface iApiCalls
     public function keyValidate($key);
     public function getRecords($zone);
     public function getMatches($answer);
-    public function replaceAnswer($new_answer);
+    public function replaceAnswer($new_answer, $change_list);
 }
